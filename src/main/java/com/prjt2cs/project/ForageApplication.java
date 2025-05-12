87bb9dc50152e7b4beb30a0ecdc03c8079a86df9
RaniaSbr <lr_sisaber@esi.dz>
2025-05-12 19:35:31 +0100

    Reporting completed

--- src/main/java/com/prjt2cs/project/ForageApplication.java
+++ src/main/java/com/prjt2cs/project/ForageApplication.java
@@ -1,8 +1,6 @@
 package com.prjt2cs.project;
 
 import com.prjt2cs.project.service.ExcelReader;
-import com.prjt2cs.project.service.MonoReader;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -18,21 +16,18 @@ public class ForageApplication {
 
 		// Récupérer le bean ExcelReader depuis le contexte
 		ExcelReader excelReader = context.getBean(ExcelReader.class);
-		MonoReader monoReader = context.getBean(MonoReader.class);
 
 		// Exemple de lecture des valeurs concaténées
 		String fileName = "18.xlsx"; // Mets bien le nom exact
-		String startColumn = "I";
-		String endColumn = "I";
-		int rowIndex = 5;
+		String startColumn = "L";
+		String endColumn = "T";
+		int rowIndex = 6;
 
 		// Appeler la méthode pour lire les cellules concaténées
-		String result = excelReader.readCellRangeConcatenated(fileName, startColumn, endColumn, rowIndex);
-		Double result2 = monoReader.readCalculatedCellAsDouble(fileName, "G", 12, 1);
+		// String result = excelReader.readCellRangeConcatenated(fileName, startColumn,
+		// endColumn, rowIndex);
 
 		// Afficher les valeurs concaténées
-		System.out.println("Valeurs concaténées : " + result);
-		System.out.println("Valeurs concaténées : " + result2);
-
+		// System.out.println("Valeurs concaténées : " + result);
 	}
 }
