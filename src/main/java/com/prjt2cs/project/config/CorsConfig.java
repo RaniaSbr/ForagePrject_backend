5a5a3fd060b114ff42c0571e021e3cc45f3a36b8
Chems Eddine CHEMINI <lc_chemini@esi.dz>
2025-06-19 15:42:44 +0100

    last

--- src/main/java/com/prjt2cs/project/config/CorsConfig.java
+++ src/main/java/com/prjt2cs/project/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // autorise React
+            .allowedOrigins("http://localhost:3001") // autorise React
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // AJOUT DE PATCH
             .allowedHeaders("*")
             .allowCredentials(true);
